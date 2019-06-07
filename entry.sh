308caee212f7fddac5dd563f8ce0aa84a5cc4378
Frank Haussmann <frank.haussmann@medneo.com>
2019-06-07 14:11:43 +0200

    Change test to check size of existing cert
    Do not only check for existence of a cert file but also ensure that it
    is not just an empty file. The file might be empty when LetsEncrypt
    throws an error during reqesting of the certificate.

--- entry.sh
+++ entry.sh
@@ -2,7 +2,7 @@
 if [ "$1" = "daemon" ];  then
  #trap \"echo stop && killall crond && exit 0\" SIGTERM SIGINT \n \
  #crond && while true; do sleep 1; done;\n \
- if [ ! -f /tls/fullchain.cer ]; then
+ if [ ! -s /tls/fullchain.cer ]; then
   --issue --dns dns_aws -d "$2"
   --install-cert -d "$2" --fullchain-file /tls/fullchain.cer --key-file /tls/key.key
  fi
