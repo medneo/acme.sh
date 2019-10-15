c4385b14e43ae0e8a38f48953ae96a015c65c59a
Frank Haussmann <frank.haussmann@medneo.com>
2019-10-15 14:26:22 +0200

    Export all possible certificate parts

--- entry.sh
+++ entry.sh
@@ -4,7 +4,7 @@ if [ "$1" = "daemon" ];  then
  #crond && while true; do sleep 1; done;\n \
  if [ ! -s /tls/fullchain.cer ]; then
   --issue --dns dns_aws -d "$2"
-  --install-cert -d "$2" --fullchain-file /tls/fullchain.cer --key-file /tls/key.key
+  --install-cert -d "$2" --fullchain-file /tls/fullchain.cer --cert-file /tl/cert.cer --key-file /tls/key.key --ca-file /tls/ca.cer
  fi
  exec crond -f
 else
